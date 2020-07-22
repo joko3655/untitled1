9584bb3ed82a016082c53d53aae69d56a64be7f3
joko <joko365@163.com>
2020-07-23 00:03:33 +0800

    Test Github

---
+++ GUI.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+import wx
+class Frame1(wx.Frame):
+   def __init__(self,parent,title):
+        wx.Frame.__init__(self, parent, title = title, pos = (100,200), size = (200,100))
+        #容纳其他组件的容器
+        panel = wx.Panel(self)
+        text1 = wx.TextCtrl(panel, value = "Hello, World!", size = (200,100))
+        self.Show(True)
+if __name__ == '__main__':
+    #创建一个应用程序对象，用于消息循环
+    app = wx.App()
+    #创建一个窗体
+    frame = Frame1(None, "Example")
+    app.MainLoop()
+#Test Github
